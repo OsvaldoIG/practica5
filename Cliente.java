82374ddae101651b6fd7843940931fe08bfecf37
LeoonardooCRZ <42351420+LeoonardooCRZ@users.noreply.github.com>
2019-09-16 20:12:47 -0500

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,22 +1,25 @@
 package practica5;
-public class Cliente(){
+public class Cliente{
     public static void main(String[] args){
         Cono c1 = new Cono(5,10);
         Esfera e1 = new Esfera(5);
         Prisma p1 = new Prisma(3,2,8);
         
         System.out.println("CONO");
-        System.out.println("Área del cono: "+c1.getArea());
+	    System.out.println(c1.toString());
+	    System.out.println("Area del cono: "+c1.getArea());
         System.out.println("Volumen del cono: "+c1.getVolumen());
         System.out.println("******");
         
         System.out.println("ESFERA");
-        System.out.println("Área de la esfera: "+e1.getArea());
-        System.out.println("Volumen de la esfera: "e1.getVolumen());
+	    System.out.println("Radio: "+e1.getRadio());
+        System.out.println("Area de la esfera: "+e1.getArea());
+        System.out.println("Volumen de la esfera: "+e1.getVolumen());
         System.out.println("******");
         
         System.out.println("PRISMA");
-        System.out.println("Área del prisma: "+p1.getArea());
+        System.out.println(p1.toString());
+	    System.out.println("Area del prisma: "+p1.getArea());
         System.out.println("Volumen del prisma: "+p1.getVolumen());
         System.out.println("******");
     }
