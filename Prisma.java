58a62c9ae21328fc16b577131bf28d3c5816b19a
LeoonardooCRZ <42351420+LeoonardooCRZ@users.noreply.github.com>
2019-09-16 20:15:07 -0500

    Update Prisma.java

--- Prisma.java
+++ Prisma.java
@@ -1,22 +1,26 @@
-public Class Prisma{
+package practica5;
+public class Prisma{
 	private int x;
 	private int y;
 	private int h;
-	public Prisma(int x, int y, int h){
+	Prisma(int x, int y, int h){
 		this.x = x;
 		this.y = y;
 		this.h = h;
 	}
 	public int getArea(){
-		return 2(x*y) + 2(x*h) + 2(y*h);
+		return 2*(x*y) + 2*(x*h) + 2*(y*h);
 	}
 	public int getVolumen(){
 		return x*y*h;
 	}
 	public String toString(){
-		return "La base es; "+x+" por "+y+" y la altura es; "+h
+		return "La base es: "+x+" por "+y+" y la altura es: "+h;
 	}
-	public setX(int x){this.x = x}
-	public setY(int y){this.y = y}
-	public setH(int h){this.h = h}
+	public void setX(int x){this.x = x;}
+	public void setY(int y){this.y = y;}
+	public void setH(int h){this.h = h;}
+	public int getX(){return x;}
+	public int getY(){return y;}
+	public int getH(){return h;}
 }
