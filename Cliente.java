4d4379be063b9fcdfbbc4e9f8122fb8cbea31875
PizzaDude007 <42350876+PizzaDude007@users.noreply.github.com>
2019-09-16 21:14:45 -0500

    Cambios menores

--- Cliente.java
+++ Cliente.java
@@ -6,20 +6,20 @@ public class Cliente{
         Prisma p1 = new Prisma(3,2,8);
         
         System.out.println("CONO");
-	    System.out.println(c1.toString());
-	    System.out.println("Area del cono: "+c1.getArea());
+	System.out.println(c1);
+	System.out.println("Area del cono: "+c1.getArea());
         System.out.println("Volumen del cono: "+c1.getVolumen());
         System.out.println("******");
         
         System.out.println("ESFERA");
-	    System.out.println("Radio: "+e1.getRadio());
+	System.out.println("Radio: "+e1);
         System.out.println("Area de la esfera: "+e1.getArea());
         System.out.println("Volumen de la esfera: "+e1.getVolumen());
         System.out.println("******");
         
         System.out.println("PRISMA");
-        System.out.println(p1.toString());
-	    System.out.println("Area del prisma: "+p1.getArea());
+        System.out.println(p1);
+	System.out.println("Area del prisma: "+p1.getArea());
         System.out.println("Volumen del prisma: "+p1.getVolumen());
         System.out.println("******");
     }
