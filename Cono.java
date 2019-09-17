8c80e23b4927ecb3af38839248eab8d64ac40f74
LeoonardooCRZ <42351420+LeoonardooCRZ@users.noreply.github.com>
2019-09-16 20:13:42 -0500

    Update Cono.java

--- Cono.java
+++ Cono.java
@@ -1,22 +1,35 @@
 package practica5;
 import java.lang.Math;
 public class Cono{
-	public int radio;
-	public int altura;
-	static Cono(int radio,int altura){
+	public double radio;
+	public double altura;
+	Cono(double radio,double altura){
 		this.radio=radio;
 		this.altura=altura;
 	}
-	public float getArea(){
-		float lado=Math.sqrt(Math.pow(radio,2)+Math.pow(altura,2));
+	public double getArea(){
+		double lado = Math.sqrt(Math.pow(radio,2)+Math.pow(altura,2));
 		return (Math.PI*radio*lado) + (Math.PI+(Math.pow(radio,2)));
 	}
 
-	public float getVolumen(){
-		return (1/3*Math.PI*Math.pow(radio,2)*altura);
+	public double getVolumen(){
+		return Math.PI*Math.pow(radio,2)*altura*0.333;
 	}
 
 	public String toString(){
 		return "El radio es "+radio+" y la altura es "+altura;
 	}
+	public void setRadio(double radio){
+		this.radio=radio;
+	}
+	public void setAltura(double altura){
+		this.altura=altura;
+	}
+	public double getRadio(){
+		return radio;
+	}
+	public double getAltura(){
+		return altura;
+	}
 }
+
